8658d0218c47be57d294e030bf79008bd1431336
trishika <35651199+trishika02@users.noreply.github.com>
2026-02-08 13:21:49 +0600

    fix

--- src/routes/api/[...path]/+server.js
+++ src/routes/api/[...path]/+server.js
@@ -4,8 +4,13 @@ export async function GET({ url, fetch }) {
 
     const response = await fetch(targetUrl.toString());
 
+    // Create a new Headers object to modify
+    const newHeaders = new Headers(response.headers);
+    newHeaders.delete('content-encoding');
+    newHeaders.delete('content-length');
+
     return new Response(response.body, {
         status: response.status,
-        headers: response.headers
+        headers: newHeaders
     });
 }
